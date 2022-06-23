0dc997a5c72ff1e17fdd310a721d4c96050aa952
Jean-Baptiste Dujardin <contact@jbdujardin.com>
2022-06-23 16:33:30 +0200

    feat(fixtures): add method to get max 20 fixtures details

--- lib/src/repositories/fixtures_repository.dart
+++ lib/src/repositories/fixtures_repository.dart
@@ -4,32 +4,34 @@ class FixturesRepository {
   FixturesRepository(this._api);
   final ApiFootballAPI _api;
 
-  Future<List<ApiFootballFixture>> getFixtures(
-          {required int leagueId, required int season}) =>
-      _api.getData(
+  Future<List<ApiFootballFixture>> getFixtures({required int leagueId, required int season}) => _api.getData(
         path: 'fixtures',
         params: {
           'league': leagueId.toString(),
           'season': season.toString(),
         },
         builder: (data) {
-          return (data['response'] as List)
-              .map((json) => ApiFootballFixture.fromJson(json))
-              .toList();
+          return (data['response'] as List).map((json) => ApiFootballFixture.fromJson(json)).toList();
         },
       );
 
-  Future<ApiFootballFixture> getFixture({required String fixtureId}) =>
-      _api.getData(
+  Future<ApiFootballFixture> getFixture({required String fixtureId}) => _api.getData(
         path: 'fixtures',
         params: {
           "id": fixtureId,
         },
         builder: (data) {
-          return (data['response'] as List)
-              .map((json) => ApiFootballFixture.fromJson(json))
-              .toList()
-              .first;
+          return (data['response'] as List).map((json) => ApiFootballFixture.fromJson(json)).toList().first;
+        },
+      );
+
+  Future<List<ApiFootballFixture>> getFixturesDetails({required List<String> fixturesIds}) => _api.getData(
+        path: 'fixtures',
+        params: {
+          "ids": fixturesIds.join('-'),
+        },
+        builder: (data) {
+          return (data['response'] as List).map((json) => ApiFootballFixture.fromJson(json)).toList();
         },
       );
 }
